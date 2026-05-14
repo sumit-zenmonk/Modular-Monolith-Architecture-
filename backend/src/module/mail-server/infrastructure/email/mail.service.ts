f2ff6fe01f95a56633c2e9fcd1c4c679d7d4c51b
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-05-14 12:02:15 +0530

    mail service resumed again after inbox-outbox pattern implemented

--- backend/src/module/mail-server/infrastructure/email/mail.service.ts
+++ backend/src/module/mail-server/infrastructure/email/mail.service.ts
@@ -12,38 +12,38 @@ export class EmailService {
     private readonly resend = new Resend(process.env.RESEND_API_KEY)
 
     async sendUserWelcome(user: UserEntity) {
-        // await this.mailerService.sendMail({
-        //     to: user.email,
-        //     // from: '"Support Team" <support@example.com>', // override default from
-        //     subject: 'Welcome to Nice App! Confirm your Email',
-        //     template: './user-registered/welcome', // `.ejs` extension is appended automatically
-        //     context: { // filling <%= %> brackets with content
-        //         name: user.name,
-        //     },
-        // });
+        await this.mailerService.sendMail({
+            to: user.email,
+            // from: '"Support Team" <support@example.com>', // override default from
+            subject: 'Welcome to Nice App! Confirm your Email',
+            template: './user-registered/welcome', // `.ejs` extension is appended automatically
+            context: { // filling <%= %> brackets with content
+                name: user.name,
+            },
+        });
     }
 
     async sendBacthEmailCreatorPostNotificationToFollower(data: CreateMailEntryPayload[]) {
-        // // fetch path
-        // const templatePath = path.join(
-        //     process.cwd(),
-        //     'src/module/mail-server/infrastructure/email/template/creator-post-to-follower/creator_post_to_follower.ejs',
-        // );
+        // fetch path
+        const templatePath = path.join(
+            process.cwd(),
+            'src/module/mail-server/infrastructure/email/template/creator-post-to-follower/creator_post_to_follower.ejs',
+        );
 
-        // // making email batch obj acc to resend
-        // const emails = await Promise.all(
-        //     data.map(async (item) => {
-        //         const html = await ejs.renderFile(templatePath, { body: item.body, });
+        // making email batch obj acc to resend
+        const emails = await Promise.all(
+            data.map(async (item) => {
+                const html = await ejs.renderFile(templatePath, { body: item.body, });
 
-        //         return {
-        //             from: 'Nice App <onboarding@resend.dev>',
-        //             to: 'sumitkumarshaab@gmail.com',//item.email
-        //             subject: 'New post from creator you follow',
-        //             html,
-        //         };
-        //     }),
-        // );
+                return {
+                    from: 'Nice App <onboarding@resend.dev>',
+                    to: 'sumitkumarshaab@gmail.com',//item.email
+                    subject: 'New post from creator you follow',
+                    html,
+                };
+            }),
+        );
 
-        // await this.resend.batch.send(emails);
+        await this.resend.batch.send(emails);
     }
 }
